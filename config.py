89ba62c3ac2a26cff5c5576fcefee76c4d2b4352
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:45:05 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -5,8 +5,16 @@ class Config(object):
     DEBUG = False
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     DEBUG = True
+    B2B_REGISTER_URL = "http://localhost:5001"
+    B2B_SEARCH_REG_URL = "http://localhost:5004"
+    B2B_SEARCH_WORK_URL = "http://localhost:5006"
+    DB2_MIGRATOR_URL = "http://localhost:5009"
+    DB2_LEGACY_URL = "http://localhost:5007"
+
+
+class PreviewConfig(Config):
     B2B_REGISTER_URL = "http://localhost:5001"
     B2B_SEARCH_REG_URL = "http://localhost:5004"
     B2B_SEARCH_WORK_URL = "http://localhost:5006"
