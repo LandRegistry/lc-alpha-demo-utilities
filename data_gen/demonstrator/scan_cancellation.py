418c0568eda92ee80d56fbe9b0607d2e1f5a3813
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-06 08:34:07 +0000

    Fake scanning

---
+++ data_gen/demonstrator/scan_cancellation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import requests
+import json
+from datetime import datetime
+
+
+response = requests.post("http://localhost:5014/document", data="{}", headers={'Content-Type': 'application/json'})
+print("Create document: " + str(response.status_code))
+data = json.loads(response.text)
+document_id = data['id']
+
+document_data = {
+    'id': document_id,
+    'metadata': {},
+    'image_paths': []
+}
+
+i = 0
+for image in ['CancForm.jpeg', 'CancEvid.jpeg']:
+    i += 1
+    file = open(image, 'rb')
+    resp = requests.post("http://localhost:5014/document/" + str(document_id) + "/image",
+                         data=file, headers={'Content-Type': 'image/jpeg'})
+    print("Upload Scan: " + str(response.status_code))
+
+
+work_data = {
+    "application_type": "WO(B)",
+    "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    "document_id": document_id,
+    "work_type": "cancel"
+}
+resp = requests.post("http://localhost:5006/workitem", data=json.dumps(work_data),
+                     headers={'Content-Type': 'application/json'})
+print("Lodge Work Item: " + str(response.status_code))
